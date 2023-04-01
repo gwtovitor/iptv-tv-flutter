f2bcabcfb72227faa3be7e51380f8a62b4219cb5
Vitor Augusto <108837467+gwtovitor@users.noreply.github.com>
2023-04-01 09:46:48 -0300

    u´p

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:iptv_flutter_app/login.dart';
 
-import 'choicepage.dart';
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
